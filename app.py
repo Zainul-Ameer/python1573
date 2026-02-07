02a2c05cfe7ad9bdfd837470303f69b517a21b98
Zainul Ameer <zainulameer@hotmail.com>
2026-02-07 13:12:57 +0530

    Added index main menu page

--- app.py
+++ app.py
@@ -7,11 +7,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/subagent-list")
-def subagent_list():
-    return render_template("subagent_list.html")
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
